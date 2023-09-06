aae69a882a18ccb7fab1ed6aa5a8e6d0e46ac424
iosdev54 <iosdev54@gmail.com>
2023-09-06 13:14:39 +0300

    Initial Commit

---
+++ SandstormSoftwareTextApp/SandstormSoftwareTextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandstormSoftwareTextAppApp.swift
+//  SandstormSoftwareTextApp
+//
+//  Created by Dmytro Grytsenko on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SandstormSoftwareTextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
